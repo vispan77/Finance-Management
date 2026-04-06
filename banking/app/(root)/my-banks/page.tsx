d3070e224937a210cb5d583e4f0a19a424125025
vispan77 <vishlpandey2332@gmail.com>
2026-04-06 14:14:43 +0530

    implement rightside and the card component

---
+++ banking/app/(root)/my-banks/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyBanks() {
+  return (
+    <div>
+      MyBanks
+    </div>
+  )
+}
+
+export default MyBanks
--- banking/app/(root)/page.tsx
+++ banking/app/(root)/page.tsx
@@ -1,11 +1,14 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 function Home() {
 
     const loggedIn = {
-        firstName: "Vishal"
+        firstName: "Vishal",
+        lastName: "Pandey",
+        email: "vishalpandey2332@gmail.com"
     }
 
     return (
@@ -31,6 +34,11 @@ function Home() {
 
             </div>
 
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 1235.45 }, { currentBalance: 1235.45 }]}
+            />
 
         </section>
     )
