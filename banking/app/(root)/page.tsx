92ae085dfb316e3be92412df820ce2db307ed794
vispan77 <vishlpandey2332@gmail.com>
2026-04-06 12:09:09 +0530

    implement folder structure and the chart bar and left bar

---
+++ banking/app/(root)/page.tsx
@@ -0,0 +1,39 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import React from 'react'
+
+function Home() {
+
+    const loggedIn = {
+        firstName: "Vishal"
+    }
+
+    return (
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.23}
+                    />
+
+
+                </header>
+
+
+            </div>
+
+
+        </section>
+    )
+}
+
+export default Home
---
+++ banking/components/DoughnutChart.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function DoughnutChart({ accounts }: DoughnutChartProps) {
+
+    const data = {
+        datasets: [
+            {
+                label: 'Banks',
+                data: [1265, 1565, 1236],
+                backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
+            }
+        ],
+        labels: ["Bank 1", "Bank 2", "Bank 3"]
+    }
+
+    return (
+        <Doughnut
+            data={data}
+            options={{
+                cutout: '60%',
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                }
+            }}
+
+        />
+    )
+}
+
+export default DoughnutChart
